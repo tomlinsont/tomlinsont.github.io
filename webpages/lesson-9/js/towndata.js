e544879618deabf3343e481330a4989840b2b5d1
Tim Tomlinson Account <tomlinsont@byui.edu>
2019-06-25 17:12:49 -0600

    json fixes

--- webpages/lesson-9/js/towndata.js
+++ webpages/lesson-9/js/towndata.js
@@ -1,23 +1,58 @@
-<script>
-            var div = document.querySelector('div');
-            var section = document.querySelector('section');
-        
-            var requestURL = 'file:///C:/Users/Tim/Documents/gitHub_files/tomlinsont.github.io/webpages/lesson-9/json/towndata.json';
+/*const myRequestObj = newXMLHttpRequest();
+
+const myRequestURL = "../json/towndata.json";
+
+let myweatherInfo;    
+myRequestObj.open('GET', requestURL);
+myRequestObj.send();
+myRequestObject.onload = function() {
+    myWeatherInfo = JSON.parse(myRequestObject.responseText);
+    console.log(myWeatherInfo);
+	
+	//make a call to the function below for each town and send the array number of the needed town. Ppon returning insert it as html inside the id for each city
+	document.getElementById("spring").innerHTML = assembleData(6);
+	document.getElementById("soda").innerHTML = assembleData(5);
+	document.getElementById("place").innerHTML = assembleData(3);
+}
+
+function assembleData(x) {
+	//make sure the town data is being loaded for the variale x
+	console.log(myWeatherInfo.towns[x]);
+	
+	//Build the html as a javascript variable and include the html tags
+	let townInfo = "<h2>"+myWeatherInfo.towns[x].name+"</h2>";
+	townInfo += "<i>"+myWeatherInfo.towns[x].motto+"</i>";
+	townInfo += "<p>Year founded: "+myWeatherInfo.towns[x].yearFounded+"</p>";
+	townInfo += "<p>Current Population: "+myWeatherInfo.towns[x].currentPopulation+"</p>";
+	townInfo += "<p>Rainfall: "+myWeatherInfo.towns[x].averageRainfall+"</p>";
+         
+	//return the variable that now has all the town information
+	return townInfo;
+}*/
+
+//let townNames = jsonObj["towns"][1],["towns"][4],["towns"][5];
+//console.log(townNames);
+                   
+            var requestURL = /*'file:///*/'C:/Users/Tim/Documents/gitHub_files/tomlinsont.github.io/webpages/lesson-9/json/towndata.json';
             
-            var request = newXMLHttpRequest();
+            var myRequestObj = newXMLHttpRequest();
             
-            request.open('GET', requestURL);
+            myRequestObj.open('GET', requestURL);
             
-            request.responseType = 'json';
-            request.send();
+         /*   request.responseType = 'json';*/
+            myRequestObj.send();
             
-            request.onload = function() {
-                var towndata = request.response;
-                populateDiv(towndata);
-                showTowns(towndata);
+            myRequestObj.onload = function() {
+                var towndata = JSON.parse(myRequestObj.response);
+              //  populateDiv(towndata);
+                console.log(towndata);
+                
+                document.getElementById("frank").innerHTML = "hi";
+                document.getElementById("green").innerHTML = showTowns(2);
+                document.getElementById("spring").innerHTML = showTowns(6);
             }
             
-            function populateDiv (jsonObj) {
+       /*     function populateDiv (jsonObj) {
                 var myTown1 = document.createElement('h2');
                 myTown1.textContent = jsonObj['towns'][0]['name'];
                 div.appendChild(myTown1);
@@ -25,32 +60,36 @@
                 var myTown1data = document.createElement('p');
                 myTown1data.textContent = jsonObj;
                 div.appendChild(myTown1data);
-            }
+            }*/
             
-            function showTowns (jsonObj) {
-                var towns = jsonObj['towns'];
-                
-                for (var i = 0; i < towns.length; i++) {
-                    var myArticle = document.createElement('section');
-                    var myH2 = document.createElement('h2');
+            function showTowns (x) {
+               // var towns = jsonObj['towns'];
+                console.log(myWeatherInfo.towns[x]);
+               
+                //for (var i = 0; i < towns.length; i++) {
+                  //  var myArticle = document.createElement('section');
+                /*    var myH2 = document.createElement('h2');
                     var myPara1 = document.createElement('p');
                     var myPara2 = document.createElement('p');
                     var myPara3 = document.createElement('p');
                     var myPara4 = document.createElement('p');
                     
-                    myH2.textContent = towns[i].name;
-                    myPara1.textContent = 'Motto: ' + towns[i].motto;
-                    myPara2.textContent = 'Year Founded: ' + towns[i].yearFounded;
-                    myPara3.textContent = 'Population: ' + towns[i].currentPopulation;
-                    myPara4.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
+                    myH2.textContent = towns[x].name;
+                    myPara1.textContent = 'Motto: ' + towns[x].motto;
+                    myPara2.textContent = 'Year Founded: ' + towns[x].yearFounded;
+                    myPara3.textContent = 'Population: ' + towns[x].currentPopulation;
+                    myPara4.textContent = 'Average Rainfall: ' + towns[x].averageRainfall;*/
+                    let townInfo = "<h2>" + myWeatherInfo.towns[x].name + "</h2>";
+                    
+                    return townInfo;
                 }
                 
-                myArticle.appendChild(myH2);
+            /*    myArticle.appendChild(myH2);
                 myArticle.appendChild(myPara1);
                 myArticle.appendChild(myPara2);
                 myArticle.appendChild(myPara3);
                 myArticle.appendChild(myPara4);
                 
-                section.appendChild(myArticle);
-            }
-        </script>
+                section.appendChild(myArticle);*/
+            
+        
