19859e01c0a14f815d005f6710931358a42bd501
Tim Tomlinson Account <tomlinsont@byui.edu>
2019-06-25 19:34:20 -0600

    json changes

--- webpages/lesson-9/js/towndata.js
+++ webpages/lesson-9/js/towndata.js
@@ -35,7 +35,8 @@ function assembleData(x) {
             
             const myRequestObj = new XMLHttpRequest();
             
-            const requestURL = "https://github.com/tomlinsont/tomlinsont.github.io/blob/master/webpages/lesson-9/json/towndata.json";
+            const requestURL = "towndata.json"
+            /*"https://github.com/tomlinsont/tomlinsont.github.io/blob/master/webpages/lesson-9/json/towndata.json";*/
             
             let towndata;
             
