8ab1354af26fbafea957ac26c48c21fb72a54059
Tim Tomlinson Account <tomlinsont@byui.edu>
2019-06-28 08:03:51 -0600

    json fixes

--- webpages/lesson-9/js/towndata.js
+++ webpages/lesson-9/js/towndata.js
@@ -1,16 +1,15 @@
-/*var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 var myRequestObj = new XMLHttpRequest();
 
 myRequestObj.open('GET', requestURL);
-myRequestObj.responseType = '';
-myRequestObj.send();*/
-import { onload, responseText } from "./json/towndata.json";
+//myRequestObj.responseType = '';
+myRequestObj.send();
 
 let myRequestData;
 
 onload = function() {
-    myRequestData = JSON.parse(responseText);    
+    myRequestData = JSON.parse(myRequestObj.responseText);    
     console.log(myRequestData);
 
     document.getElementById("frank").innerHTML = completeTownData(0);
