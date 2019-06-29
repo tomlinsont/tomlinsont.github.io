75d58aee3d9cc435c94fd3454f6c317c49375d80
Tim Tomlinson Account <tomlinsont@byui.edu>
2019-06-29 14:17:39 -0600

    api fix

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -1,14 +1,12 @@
 let weatherRequest = new XMLHttpRequest();
 
-var apiURLstring = 'http://samples.openweathermap.org/data/2.5/forecast?id=5061036&units=imperial&appid=dd11f546924470c26cb3acba8d198320';
+var apiURLstring = 'https://samples.openweathermap.org/data/2.5/forecast?id=5061036&units=imperial&appid=dd11f546924470c26cb3acba8d198320';
 
 weatherRequest.open('GET', apiURLstring, true);
 
 weatherRequest.send();
 
-let weatherData;
-
 weatherRequest.onload = function() {
-    weatherData = JSON.parse(weatherRequest.responseText);
+    let weatherData = JSON.parse(weatherRequest.responseText);
     console.log (weatherData);
 }
