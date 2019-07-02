6db0dd62aa79fd636a3ecf44c6e590fffe9f487a
Tim Tomlinson Account <tomlinsont@byui.edu>
2019-07-02 13:03:58 -0600

    current Weather

--- webpages/lesson-10/js/weatherinfo.js
+++ webpages/lesson-10/js/weatherinfo.js
@@ -1,6 +1,6 @@
 let weatherRequest = new XMLHttpRequest();
 
-var apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5061036&units=imperial&appid=dd11f546924470c26cb3acba8d198320';
+var apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=dd11f546924470c26cb3acba8d198320';
 
 weatherRequest.open('GET', apiURLstring, true);
 
@@ -10,5 +10,6 @@ weatherRequest.onload = function() {
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log (weatherData);
 
-   /* document.getElementById("currenttemp").innerHTML = weatherData.main.temp;*/
+    document.getElementById("lowtemp").innerHTML = weatherData.main.temp;
+    document.getElementById("hightemp").innerHTML = weatherData.main.temp
 }
