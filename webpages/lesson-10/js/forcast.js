f2a501e9f0da9e93aacc830c786e0407093c1858
Tim Tomlinson Account <tomlinsont@byui.edu>
2019-07-06 14:57:50 -0600

    forcast days

--- webpages/lesson-10/js/forcast.js
+++ webpages/lesson-10/js/forcast.js
@@ -5,7 +5,7 @@ const day = new Date();
 const newDay = day.getDay();
 console.log(newDay);
 
-/*
+
 let forcastRequest = new XMLHttpRequest();
 
 var apiURLstring = '//api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=dd11f546924470c26cb3acba8d198320';
@@ -18,5 +18,6 @@ forcastRequest.onload = function() {
     let forcastData = JSON.parse(forcastRequest.responseText);
     console.log (forcastData);
 
-   
-}*/
+    let list = forcastData.list;
+    console.log(list.length);
+}
