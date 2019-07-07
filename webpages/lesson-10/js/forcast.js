f1a701d4c206185246f21110527ed3e6148a3ce5
Tim Tomlinson Account <tomlinsont@byui.edu>
2019-07-06 19:22:38 -0600

    update

--- webpages/lesson-10/js/forcast.js
+++ webpages/lesson-10/js/forcast.js
@@ -30,7 +30,7 @@ forcastRequest.onload = function() {
         let forcastTime = forcastList[i].dt_txt;
         if (forcastTime.match(/18:00:00/gi))
         {
-            if (newDay === 7)
+            if (newDay === 6)
             {
                 newDay = 0;
             }
