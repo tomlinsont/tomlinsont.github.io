1340345457699f978e1bcec40fe1326355582b49
Tim Tomlinson Account <tomlinsont@byui.edu>
2019-07-07 18:22:25 -0600

    town events info

--- webpages/lesson-10/js/townevents.js
+++ webpages/lesson-10/js/townevents.js
@@ -7,7 +7,7 @@ eventsRequestObj.open('GET', apiURLstring, true);
 eventsRequestObj.send();
 
 eventsRequestObj.onload = function() {
-    let townEvents = JSON.parse(eventsRequestObj, this.responseText);
+    let townEvents = JSON.parse(eventsRequestObj.responseText);
 
     console.log(townEvents);
 }
