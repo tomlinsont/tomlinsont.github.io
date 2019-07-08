5b33a0e1b80e2770ea59402af64dd8a1a697aa46
Tim Tomlinson Account <tomlinsont@byui.edu>
2019-07-08 15:58:08 -0600

    updates

--- webpages/lesson-10/js/currentdate.js
+++ webpages/lesson-10/js/currentdate.js
@@ -1,14 +1,14 @@
     // Array - for each day of the month. 
-    var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    let monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
 
     // Array for the days of the week.
-   /* var dayNames = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];*/
+    let dayNames = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
 
     // Create a date object
-	var newDate = new Date();
+	const newDate = new Date();
 			
     // var formattedDate is string.  assigned the concatenated elements //below
 
-	var formattedDate = dayNames[newDate.getDay()] + ',' +' ' + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear();
+	let formattedDate = dayNames[newDate.getDay()] + ',' +' ' + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear();
 			
 	document.getElementById("outputdiv").innerHTML = formattedDate;
