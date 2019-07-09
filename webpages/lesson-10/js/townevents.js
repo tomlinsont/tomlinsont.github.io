c5290c323e76eb9039f4fda356997aed99f6aa39
Tim Tomlinson Account <tomlinsont@byui.edu>
2019-07-09 15:26:31 -0600

    update events

--- webpages/lesson-10/js/townevents.js
+++ webpages/lesson-10/js/townevents.js
@@ -12,12 +12,17 @@ eventsRequestObj.onload = function() {
 
     let eventsList = townEvents.towns;
     document.getElementById("preston_events").innerHTML = eventsList[4].name;
-    document.getElementById("fish_haven_events").innerHTML = eventsList[1].name;
-    document.getElementById("soda_springs_events").innerHTML = eventsList[5].name;
+    document.getElementById("preston_events").innerHTML = eventsList[4].events;
     console.log(eventsList[4].name);
     console.log(eventsList[4].events);
+
+    document.getElementById("fish_haven_events").innerHTML = eventsList[1].name;
+    document.getElementById("fish_haven_events").innerHTML = eventsList[1].events;
     console.log(eventsList[1].name);
     console.log(eventsList[1].events);
+
+    document.getElementById("soda_springs_events").innerHTML = eventsList[5].name;
+    document.getElementById("soda_springs_events").innerHTML = eventsList[5].events;
     console.log(eventsList[5].name);
     console.log(eventsList[5].events);
 }
